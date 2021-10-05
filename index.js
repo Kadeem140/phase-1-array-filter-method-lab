4043055d7acd45f995a0b5b1d04234fa8e21ae24
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-10-05 19:27:20 -0400

    Incompleted

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching( drivers, name ){
+    // console.log(drivers, 'Drivers')
+    const filteredNames = []
+    drivers.filter( function (name){
+        // console.log(name, "name")
+        if(name == "Bobby"){
+            filteredNames.push(name)
+            console.log(filteredNames, 'Filtered String')
+            return filteredNames
+        }
+    })
+}
