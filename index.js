c06f8be5191c53d8fedf31c282dc79ad6a01b319
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-02-22 21:57:59 -0500

    MVP

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // Code your solution here
-function findMatching( drivers, name ){
-    // console.log(drivers, 'Drivers')
-    const filteredNames = []
-    drivers.filter( function (name){
-        // console.log(name, "name")
-        if(name == "Bobby"){
-            filteredNames.push(name)
-            console.log(filteredNames, 'Filtered String')
-            return filteredNames
-        }
+
+const Name = "Bobby"
+
+function findMatching(drivers, Name){
+    const filteredNames = drivers.filter((driver) =>{
+        return driver == Name
     })
+    return filteredNames
 }
+
+function fuzzyMatch(drivers, name){
+    console.log(name.split(''), "split")
+    const splitString = name.split('')
+    const filteredDrivers = drivers.filter((driver) =>{
+        return driver == splitString
+    })
+    return filteredDrivers
+}
+
+function matchName(driverArrOfObj, arg){
+    
+    const driverNames = driverArrOfObj.forEach(element => {
+        return [element.name]
+    });
+    // driverNames.filter((driver) => {
+    //     return driver == arg
+    // })
+    if(driverNames == arg){
+        return driverNames
+    }
+    // return driverNames
+
+
+}
+
